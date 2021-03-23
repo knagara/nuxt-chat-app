429afde89ecc852306747b08e80e0c54d394b75d
knagara <nagara.keita@gmail.com>
2021-03-23 23:41:55 +0900

    auth

--- nuxt.config.js
+++ nuxt.config.js
@@ -20,10 +20,13 @@ export default {
 
   // Global CSS: https://go.nuxtjs.dev/config-css
   css: [
+    '~/assets/css/tailwind.css'
   ],
 
   // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
   plugins: [
+    '~/plugins/firebase',
+    '~/plugins/auth',
   ],
 
   // Auto import components: https://go.nuxtjs.dev/config-components
